bbe9ea4e349cc519a5234519fac2b2d08280d65c
alopezp90 <alopezp90@gmail.com>
2020-11-29 13:20:46 +0100

    Estructura y solicitud de parametros

--- src/main/java/serpientes/Programa.java
+++ src/main/java/serpientes/Programa.java
@@ -11,16 +11,58 @@ import javax.swing.JLabel;
 public class Programa {
 
     public static final ImageIcon ICONO = new ImageIcon("src/main/resources/snake_96x.jpg");
-    
+    public static final int NMAX = 100;
+
+    public static int[] lectura;
+
     public static void main(String[] args) {
-        
-        int opcion = JOptionPane.showOptionDialog(
-                    null,
-                    "Pueba",
-                    "Menú Principal",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
+
+        do {
+            int n = solicitaDato("cantidad de mediciones de temperatura. (1-" + NMAX + ")", 1, NMAX);
+            int k = solicitaDato("número de mediciones soportadas bajo umbral de temperatura. (0-" + (n - 2) + ")", 0, n - 2);
+            tipoListado();
+            buscaOptimo();
+        } while (menuSalir());
+
+    }
+
+    public static int solicitaDato(String mensaje, int min, int max) {
+        int valor;
+        do {
+            Object objeto = JOptionPane.showInputDialog(null,
+                    "Introduce " + mensaje,
+                    "Dato necesario:",
+                    JOptionPane.OK_OPTION,
                     ICONO,
-                    new Object[]{"Modo Limpieza", "Modo Carga", "Configuración", "Salir"}, null);
+                    null,
+                    null);
+            String texto = objeto.toString();
+            if (isParsable(texto)) {
+                valor = Integer.parseInt(texto);
+            } else {
+                valor = max + 1;
+            }
+            if (valor < min || valor > max) {
+                JOptionPane.showOptionDialog(
+                        null,
+                        "El valor introducino no es válido.\n"
+                        + "Valores válidos: " + min + "-" + max + ".",
+                        "ERROR",
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.ERROR_MESSAGE,
+                        ICONO,
+                        new Object[]{"Ok"}, null);
+            }
+        } while (valor < min || valor > max);
+        return valor;
+    }
+
+    public static boolean isParsable(String texto) {
+        try {
+            Integer.parseInt(texto);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
