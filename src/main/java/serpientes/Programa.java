7498264bf9cf756220e9e1dacb370f670b2f5b5e
alopezp90 <alopezp90@gmail.com>
2020-11-29 12:58:12 +0100

    Estructura lógica principal

--- src/main/java/serpientes/Programa.java
+++ src/main/java/serpientes/Programa.java
@@ -1,9 +1,26 @@
 package serpientes;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+
 /**
- * @author Alberto López Puertas 
+ * @author Alberto López Puertas
  * <alopezp90@gmail.com>
  */
 public class Programa {
 
+    public static final ImageIcon ICONO = new ImageIcon("src/main/resources/snake_96x.jpg");
+    
+    public static void main(String[] args) {
+        
+        int opcion = JOptionPane.showOptionDialog(
+                    null,
+                    "Pueba",
+                    "Menú Principal",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    ICONO,
+                    new Object[]{"Modo Limpieza", "Modo Carga", "Configuración", "Salir"}, null);
+    }
 }
