1b988d6b2ce4f42232266e843017bb12cc533a82
alopezp90 <alopezp90@gmail.com>
2020-11-29 20:42:17 +0100

    ACABADO

--- src/main/java/serpientes/Programa.java
+++ src/main/java/serpientes/Programa.java
@@ -16,18 +16,20 @@ public class Programa {
     public static final int NMAX = 100;
 
     //Declaracion de variables
-    public static String lectura = "";
+    public static String lectura;
     public static int n, k;
     public static Candidato[] candidato;
 
     //Método main con estructura lógica principal del programa
     public static void main(String[] args) {
         do {
+            lectura = "";
             n = solicitaDato("cantidad de mediciones de temperatura. (1-" + NMAX + ")", 1, NMAX);
             k = solicitaDato("número de mediciones soportadas bajo umbral de temperatura. (0-" + n + ")", 0, n);
             tipoListado(n);
             creaCandidatos();
             limpiaCandidatos();
+            resultado();
         } while (!menuSalir());
 
     }
@@ -127,9 +129,9 @@ public class Programa {
         }
     }
 
-    //Inicializa el array de candidatos, busca k+1 ceros consecutivos y parte lecturas
+    //Inicializa el array de candidatos, busca k+1 ceros consecutivos y fragmenta el String lecturas
     public static void creaCandidatos() {
-        candidato = new Candidato[n / k];
+        candidato = new Candidato[n ];
         String ceros = "";
         for (int i = 0; i < k + 1; i++) {
             ceros = ceros + 0;
@@ -160,17 +162,55 @@ public class Programa {
 //            }
 //        }
     }
-    
-    public static void limpiaCandidatos(){
+
+    //Elimina los ceros iniciales y finales de los candidatos, modificando indice y longitud en consecuencia
+    public static void limpiaCandidatos() {
         for (int i = 0; i < candidato.length; i++) {
-                if (candidato[i] != null) {
-                    while (candidato[i].getSubstring().charAt(0) == '0'){
-                        candidato[i].setSubstring(candidato[i].getSubstring().substring(1));
-                        candidato[i].setIndice(candidato[i].getIndice()+1);
-                        candidato[i].setLongitud(candidato[i].getLongitud()-1);
-                    }
+            if (candidato[i] != null) {
+                while (candidato[i].getSubstring().startsWith("0")) {
+                    candidato[i].setSubstring(candidato[i].getSubstring().substring(1));
+                    candidato[i].setIndice(candidato[i].getIndice() + 1);
+                    candidato[i].setLongitud(candidato[i].getLongitud() - 1);
+                }
+                while (candidato[i].getSubstring().endsWith("0")) {
+                    candidato[i].setSubstring(candidato[i].getSubstring().substring(0, candidato[i].getLongitud() - 1));
+                    candidato[i].setLongitud(candidato[i].getLongitud() - 1);
                 }
             }
+        }
+//        for (int j = 0; j < candidato.length; j++) {
+//            if (candidato[j] != null) {
+//                System.out.println(candidato[j].getSubstring());
+//                System.out.println(candidato[j].getIndice());
+//                System.out.println(candidato[j].getLongitud());
+//            }
+//        }
+    }
+
+    //Comprueba cual es el candidato más largo e imprime por pantalla la información para el usuario
+    public static void resultado() {
+        int elMejor = 0;
+        for (int i = 1; i < candidato.length; i++) {
+            if (candidato[i] != null) {
+                if (candidato[i].getLongitud() > candidato[elMejor].getLongitud()) {
+                    elMejor = i;
+                }
+            }
+        }
+        String mensaje = "<html>El periodo óptimo tiene longitud <font color='green'>" + candidato[elMejor].getLongitud() + "</font>"
+                + " y comienza en la medición <font color='red'>" + (candidato[elMejor].getIndice() + 1) + "</font>:<br/>"
+                + "<p align='center'>" + lectura.substring(0, candidato[elMejor].getIndice()) + "<font color='red'>"
+                + candidato[elMejor].getSubstring() + "</font>" + lectura.substring(candidato[elMejor].getIndice() + candidato[elMejor].getLongitud())
+                + "</p></html>";
+
+        JOptionPane.showOptionDialog(
+                null,
+                new JLabel(mensaje),
+                "Resultado",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                ICONO,
+                new Object[]{"Ok"}, "Ok");
     }
 
     //Ventana de confirmación de saida del programa
