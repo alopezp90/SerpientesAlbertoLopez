d0e131d0bee875d3459aacc1169b3e6d6761e4a6
alopezp90 <alopezp90@gmail.com>
2021-01-13 12:35:25 +0100

    Agrega Javadoc

--- src/main/java/serpientes/Candidato.java
+++ src/main/java/serpientes/Candidato.java
@@ -1,27 +1,44 @@
 package serpientes;
 
 /**
+ * Esta clase guarda informacion sobre las subcadenas que son canditas a ser
+ * optimas para la serpiente.
+ *
  * @author Alberto López Puertas
  * <alopezp90@gmail.com>
  */
 public class Candidato {
 
+    //instaciacion de variables
     private String substring;
     private int indice;
     private int longitud;
 
-    public Candidato(String substring, int indice, int longitud) {
-        this.substring = substring;
-        this.indice = indice;
-        this.longitud = longitud;
-    }
-
+    /**
+     * Constructor por defecto de Candidato. Compuesto por un String vacio,
+     * indice = 0 y longitud = 0.
+     */
     public Candidato() {
         this.substring = "";
         this.indice = 0;
         this.longitud = 0;
     }
 
+    /**
+     * Constructor parametrizado de Candidato.
+     *
+     * @param substring String candidata a ser optima.
+     * @param indice int con la posicion del primer caracter del Candidato
+     * dentro de la cadena principal.
+     * @param longitud int con la longitud de la subcadena
+     */
+    public Candidato(String substring, int indice, int longitud) {
+        this.substring = substring;
+        this.indice = indice;
+        this.longitud = longitud;
+    }
+
+    //metodos getter y setter
     public String getSubstring() {
         return substring;
     }
@@ -45,6 +62,4 @@ public class Candidato {
     public void setLongitud(int longitud) {
         this.longitud = longitud;
     }
-
-    
 }
--- src/main/java/serpientes/Programa.java
+++ src/main/java/serpientes/Programa.java
@@ -6,37 +6,61 @@ import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 
 /**
+ * Serpientes de cascabel. Dado un numero de mediciones entre 1 y 100, compuesto
+ * por un String de unos y ceros; determinar la serie mas larga de mediciones
+ * que, commenzando y acabando por 1, no contengan mas de un numero dado de
+ * ceros consecutivos.
+ *
+ * @see <a href='https://www.aceptaelreto.com/problem/statement.php?id=444'>
+ * Acepta el Reto</a>
  * @author Alberto López Puertas
  * <alopezp90@gmail.com>
  */
 public class Programa {
 
-    //Declaracion de constantes e icono
+    //declaracion de la constante ICONO para usar en JOptionPane (solo visual)
     public static final ImageIcon ICONO = new ImageIcon("src/main/resources/snake_96x.jpg");
+
+    //declaracion de la constante que limita la longitud maxima del String
     public static final int NMAX = 100;
 
-    //Declaracion de variables
-    public static String lectura;
-    public static int n, k;
-    public static Candidato[] candidato;
+    //declaracion de variables
+    public static String lectura;       //String con la serie de medicionnes
+    public static int n, k;             //Int con longitud del String y ceros consecutivos permitidos
+    public static Candidato[] candidato;//Array de Candidatos que almacena las posibles subcadenas y las analiza 1 a 1
 
-    //Método main con estructura lógica principal del programa
+    //metodo main con estructura lógica principal del programa
     public static void main(String[] args) {
+        //bucle para repetir el programa mientras el usuario lo desee
         do {
+            //inicializa vacio el String en cada vuelta
             lectura = "";
+            //solicita los parametros n y k mediante el metodo solicitaDato()
             n = solicitaDato("cantidad de mediciones de temperatura. (1-" + NMAX + ")", 1, NMAX);
             k = solicitaDato("número de mediciones soportadas bajo umbral de temperatura. (0-" + n + ")", 0, n);
+            //da opcion al usuario a introducir manualmente el String o aleatoriamente
             tipoListado(n);
+            //inicializa el array de Candidato y guarda en el las subcadenas posibles
             creaCandidatos();
+            //quita los ceros iniciales y finales que han podido quedar en los substring
             limpiaCandidatos();
+            //busca entre los candidatos el optimo y muestra la solucion
             resultado();
+            //permite al usuario repetir la ejecucion o no
         } while (!menuSalir());
 
     }
 
-    //Método para solicitud de datos por teclado
+    /**
+     * Metodo para introducir parametros enteros mediante JOptionPane.
+     *
+     * @param mensaje mensaje a mostrar en el panel
+     * @param min valor minimo permitido para el parametro
+     * @param max valor maximo parmitido para el parametro
+     * @return int con el valor del parametro introducido por el usuario
+     */
     public static int solicitaDato(String mensaje, int min, int max) {
-        int valor = max + 1;     //valor inicializado fuera del rango del bucle para repetir lo por defecto
+        int valor = max + 1;     //valor inicializado fuera del rango del bucle para repetirlo por defecto
         do {
             Object objeto = JOptionPane.showInputDialog(null,
                     "Introduce " + mensaje,
@@ -45,7 +69,7 @@ public class Programa {
                     ICONO,
                     null,
                     null);
-            if (objeto != null) {       //Obliga a introducir algun valor, no permite salida con "cancelar" o cerrar ventana
+            if (objeto != null) {   //Obliga a introducir algun valor, no permite salida con "cancelar" o cerrar ventana
                 String texto = objeto.toString();
                 if (isParsable(texto)) {
                     valor = Integer.parseInt(texto);
@@ -67,7 +91,13 @@ public class Programa {
         return valor;
     }
 
-    //Método auxiliar para evitar excepciones en entrada por teclado
+    /**
+     * Metodo de comprobacion del parseo de enteros. Metodo auxiliar para
+     * solicitaDato(), filtra que la entrada sea parseable.
+     *
+     * @param texto entrada de JOptioPane
+     * @return true si es parseable como entero, false en caso contrario
+     */
     public static boolean isParsable(String texto) {
         try {
             Integer.parseInt(texto);
@@ -77,7 +107,13 @@ public class Programa {
         }
     }
 
-    //Método para seleccionar introducir datos manualmente o aleatorios
+    /**
+     * Metodo para rellenar las lecturas. El metodo da a elegir entre introducir
+     * el String de lecturas manualmente o random y llama al metodo adecuado.
+     * Finalmente, muestra el listado una vez relleno.
+     *
+     * @param n numero de caracteres en el String
+     */
     public static void tipoListado(int n) {
         int opcion;
         do {
@@ -98,8 +134,10 @@ public class Programa {
                     listadoRandom(n);
                     break;
             }
+            //Mantiene en bucle el JOptionPane hasta que haya elegido una opcion
         } while (opcion != 0 && opcion != 1);
 
+        //codigo html para el Label del mensaje en JOptionPane (solo visual)
         String mensaje = "<html>Las mediciones obtenidas son:<br/>"
                 + "<p align='center'>" + lectura + "</p></html>";
         JOptionPane.showOptionDialog(
@@ -112,7 +150,12 @@ public class Programa {
                 new Object[]{"Ok"}, "Ok");
     }
 
-    //Método para introducir listado manualmente
+    /**
+     * Metodo para introducir manualmente las lecturas. Mediante el metodo
+     * solicitaDato() va pidiendo uno a uno los valores.
+     *
+     * @param n numero de caracteres necesarios
+     */
     public static void listadoManual(int n) {
         for (int i = 0; i < n; i++) {
             lectura = lectura + solicitaDato("medición de temperatura " + (i + 1) + ":\n"
@@ -121,7 +164,12 @@ public class Programa {
         }
     }
 
-    //Método para introducir listado aleatoriamente
+    /**
+     * Metodo para introducir aleatoriamente las lecturas. Genera un String de n
+     * caracteres 0 y 1.
+     *
+     * @param n numero de caracteres necesarios
+     */
     public static void listadoRandom(int n) {
         Random random = new Random();
         for (int i = 0; i < n; i++) {
@@ -129,20 +177,35 @@ public class Programa {
         }
     }
 
-    //Inicializa el array de candidatos, busca k+1 ceros consecutivos y fragmenta el String lecturas
+    //este metodo deberia estar en Candidato.java
+    /**
+     * Metodo para fraccionar el String en substrings Candidato. La condicion a
+     * cumplir es que no contengan k+1 ceros consecutivos, por lo tanto, primero
+     * se crea un String que contenga dichos k+1 ceros y posteriormente se
+     * splittea el String antes y despues de dichos ceros. Guardando en cada
+     * candidato el substring resultante, su posicion dentro del String y su
+     * longitud.
+     */
     public static void creaCandidatos() {
-        candidato = new Candidato[n ];
+        //inicializa el array de Candidato
+        candidato = new Candidato[n];
+        //crea la cadena de k+1 ceros
         String ceros = "";
         for (int i = 0; i < k + 1; i++) {
             ceros = ceros + 0;
         }
+        //splittea el String y lo guarda temporalmente en el array partes
         String[] partes = lectura.split(ceros);
+
+        //recorre el array partes mientras crea los Candidatos y guarda cada substring en ellos
         int i = 0;
         for (String parte : partes) {
             candidato[i] = new Candidato();
             candidato[i].setSubstring(parte);
             i++;
         }
+        //recorre el String buscando la posicion de los candidatos para guardar dicha informacion
+        //esta parte del algoritmo es ineficaz, y problablemente pueda lograrse de forma mas sencilla
         int indice = 0;
         String tmp;
         for (int j = 0; j < candidato.length; j++) {
@@ -153,17 +216,16 @@ public class Programa {
                 indice = candidato[j].getLongitud() + candidato[j].getIndice();
             }
         }
-
-//        for (int j = 0; j < candidato.length; j++) {
-//            if (candidato[j] != null) {
-//                System.out.println(candidato[j].getSubstring());
-//                System.out.println(candidato[j].getIndice());
-//                System.out.println(candidato[j].getLongitud());
-//            }
-//        }
     }
 
-    //Elimina los ceros iniciales y finales de los candidatos, modificando indice y longitud en consecuencia
+    //este metodo deberia estar en Candidato.java
+    /**
+     * Metodo que elimina ceros iniciales y finales de los Candidatos. Puesto
+     * que el split solo tiene en cuenta k+1 ceros, cualquier otro cero que
+     * hubiera se queda guardado en los substrings, este metodo los repasa y
+     * cambia su indice y longitud en consecuencia.
+     */
+    //este metodo, de nuevo, es muy ineficiente, no deberia ser necesario si creaCandidatos() fuera mejor.
     public static void limpiaCandidatos() {
         for (int i = 0; i < candidato.length; i++) {
             if (candidato[i] != null) {
@@ -178,16 +240,12 @@ public class Programa {
                 }
             }
         }
-//        for (int j = 0; j < candidato.length; j++) {
-//            if (candidato[j] != null) {
-//                System.out.println(candidato[j].getSubstring());
-//                System.out.println(candidato[j].getIndice());
-//                System.out.println(candidato[j].getLongitud());
-//            }
-//        }
     }
 
-    //Comprueba cual es el candidato más largo e imprime por pantalla la información para el usuario
+    /**
+     * Metodo que busca de entre todos los candidatos el optimo y lo muestra
+     * mediante JOptionPane.
+     */
     public static void resultado() {
         int elMejor = 0;
         for (int i = 1; i < candidato.length; i++) {
@@ -197,6 +255,7 @@ public class Programa {
                 }
             }
         }
+        //codigo html para el Label del mensaje (solo visual)
         String mensaje = "<html>El periodo óptimo tiene longitud <font color='green'>" + candidato[elMejor].getLongitud() + "</font>"
                 + " y comienza en la medición <font color='red'>" + (candidato[elMejor].getIndice() + 1) + "</font>:<br/>"
                 + "<p align='center'>" + lectura.substring(0, candidato[elMejor].getIndice()) + "<font color='red'>"
@@ -213,7 +272,11 @@ public class Programa {
                 new Object[]{"Ok"}, "Ok");
     }
 
-    //Ventana de confirmación de saida del programa
+    /**
+     * Metodo para elegir si salir del programa.
+     *
+     * @return true para salir, false para repetir.
+     */
     public static boolean menuSalir() {
         int opcion = JOptionPane.showOptionDialog(
                 null,
